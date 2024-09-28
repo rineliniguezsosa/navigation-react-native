c1cc4d1b0f0e346402dcce8169003305354350be
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 13:16:55 -0600

    [FEAT] Importando react-native-gesture-handler en el archivo principal App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
