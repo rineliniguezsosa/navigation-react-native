39113dd8b7927b433c6ffbafaa81b8ae21540893
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 16:13:11 -0600

    [FEAT] Estableciendo en el stack navigator la ruta "ProductScreen" en el archivo StackNavigator

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,6 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { HomeScreen,ProductsScreen,SettingScreen } from '../screens';
+import { HomeScreen,ProductsScreen,SettingScreen,ProductScreen } from '../screens';
 
 
 const Stack = createStackNavigator();
@@ -10,6 +10,7 @@ export const StackNavigator = () => {
     <Stack.Navigator>
        <Stack.Screen name="HomeScreen" component={HomeScreen} />
        <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
+       <Stack.Screen name="ProductScreen" component={ProductScreen} />
        <Stack.Screen name="SettingScreen" component={SettingScreen} />
     </Stack.Navigator>
   );
