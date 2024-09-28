3e027cfe572fb8aef780ee012f2ecf354f325539
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 17:34:41 -0600

    [FEAT] Cambios en el archivo ProductScreen.tsx
    - deshabilitando la exhaustiva dependencia de el hook useEffect
    - importando el hook useEffect
    - importando el useNavigation
    - creando la variable navigation donde pasamos por referencia la función useNavigation
    - establecemos la función useEffect para que solo se actualice una sola vez y de la navegación establecemos las opciones para cambiar el title del stack navigator por el nombre del param

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
 import { View,Text } from 'react-native';
-import { useRoute,RouteProp } from '@react-navigation/native';
+import { useRoute,useNavigation, RouteProp } from '@react-navigation/native';
 import { RootStackParamList } from '../../../types/interfaces';
 import { globalStyles } from '../../styles/globalStyles';
 
 export const ProductScreen = () => {
     const params = useRoute<RouteProp<RootStackParamList,'ProductScreen'>>().params;
+    const navigation = useNavigation();
     console.log(params);
+
+    useEffect(() => {
+        navigation.setOptions({
+            title:params.name,
+        });
+    }, []);
+
   return (
     <View style={globalStyles.container}>
         <Text>ProductScreen</Text>
