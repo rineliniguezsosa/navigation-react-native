94738701b0b01f8eb3e7f4891cf1df405331cfa1
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 16:43:01 -0600

    [FEAT] Creando el type RootStackParamList para tipar las rutas del stack navigator
    - props HomeScreen,ProductsScreen, SettingScreen como undefined
    - ProductScreen tipado como un objeto con las props id numer y name como string

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -2,3 +2,10 @@ export interface PrimaryButtonProps {
     label:string;
     onPress:()=> void
 }
+
+export type RootStackParamList = {
+    HomeScreen: undefined,
+    ProductsScreen: undefined,
+    ProductScreen: {id:number;name:string},
+    SettingScreen: undefined,
+};
