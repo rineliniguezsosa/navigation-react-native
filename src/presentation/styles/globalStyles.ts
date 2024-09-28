0ee10d178da0df1c7e1f8bd00634b1fef6b0a708
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 13:54:38 -0600

    [FEAT] Pegando las clases container, primaryButton y buttonText para el css de la app
    - importando los colores globalColors

--- src/presentation/styles/globalStyles.ts
+++ src/presentation/styles/globalStyles.ts
@@ -1,5 +1,22 @@
 import { StyleSheet } from 'react-native';
+import { globalColors } from './colors';
 
 export const globalStyles = StyleSheet.create({
-
+    container:{
+        flex:1,
+        padding:20,
+        backgroundColor:globalColors.background,
+    },
+    primaryButton: {
+        backgroundColor: globalColors.primary,
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+        width: '100%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: globalColors.background,
+        fontSize: 18,
+    },
 });
