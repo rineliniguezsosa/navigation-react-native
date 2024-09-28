85be5060f7c18c9749922dd693658061cc94866c
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 14:18:59 -0600

    [FEAT] Cambios en el archivo PrimaryButton
    - importando la interface PrimaryButtonProps
    - importando los estilos globalStyles
    - desestruturando las props label y onPress, tipando las props mediante la interface PrimaryButtonProps
    - estableciendo las props style con las clases primaryButton y buttonText
    - estableciendo la prop label como titulo del componente Text

--- src/presentation/components/Button/PrimaryButton.tsx
+++ src/presentation/components/Button/PrimaryButton.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Pressable,Text } from 'react-native';
+import { PrimaryButtonProps } from '../../../types/interfaces';
+import { globalStyles } from '../../styles/globalStyles';
 
-export const PrimaryButton = () => {
+export const PrimaryButton = ({label,onPress}:PrimaryButtonProps) => {
   return (
-    <Pressable>
-        <Text>PrimaryButton</Text>
+    <Pressable style={globalStyles.primaryButton} onPress={onPress}>
+        <Text style={globalStyles.buttonText}>{label}</Text>
     </Pressable>
   );
 };
