42fa0b7eb4feab47e417b86472660c57502f5125
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 02:04:21 -0600

    [FEAT] Cambios en el archivo App.tsx
    - importando el componente NavigationContainer  para envolver la aplicación

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export const App = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>App</Text>
-    </View>
+    </NavigationContainer>
   );
 };
