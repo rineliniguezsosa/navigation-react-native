5a18e6356009b5b087516b708a4c56bf24bcaf7b
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 13:32:59 -0600

    [FEAT] Creando el stack navigator para los productos donde establecimos por nombre el stack ProductsScreen el cual nos regresará la vista ó componente  ProductsScreen

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,6 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { HomeScreen } from '../screens';
+import { HomeScreen,ProductsScreen } from '../screens';
 
 
 const Stack = createStackNavigator();
@@ -9,6 +9,7 @@ export const StackNavigator = () => {
   return (
     <Stack.Navigator>
        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+       <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
     </Stack.Navigator>
   );
 };
