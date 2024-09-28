6f577c418eef201e7f4e1b7810fcbf68501f188a
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 17:23:08 -0600

    [FEAT] Camios en el archivo ProductScreen.tsx
    -  importando useRoute para obetener los parametros y RouteProp para tipar los parametros
    - importamos la interface RootStackParamList
    - importamos globalStyles para el css
    - creamos  la variable params donde hacemos el uso del hook useRoute para obtener los params y establecemos el type RouteProp para tipar los parametros de la ruta "ProductScreen"
    - establecemos un log para observar los params
    - establecemos css mediante el globalStyles para usar la clase container
    - establecenmos el componente Text para observar los parametros obtenidos

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View,Text } from 'react-native';
+import { useRoute,RouteProp } from '@react-navigation/native';
+import { RootStackParamList } from '../../../types/interfaces';
+import { globalStyles } from '../../styles/globalStyles';
 
 export const ProductScreen = () => {
+    const params = useRoute<RouteProp<RootStackParamList,'ProductScreen'>>().params;
+    console.log(params);
   return (
-    <View>
+    <View style={globalStyles.container}>
         <Text>ProductScreen</Text>
+
+        <Text>{params.id} - {params.name}</Text>
     </View>
   );
 };
