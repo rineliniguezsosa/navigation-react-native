be2e764bce0a8414090f3c7fa28ed2bd270ab657
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 14:27:30 -0600

    [FEAT] Multiples cambios en el archivo HomeScreen.tsx
    - removiendo la importación del componente Pressable
    - importando el componente PrimaryButton
    - importando useNavigation  de react-native similar a react router
    - creando la variable navigation donde pasamos por referencia la funcionalidad de useNavigation
    - estableciendo el componente PrimaryButton con las props label con el titulo Productos, pasamos la prop onPress donde establecemos una función de flecha para usar el navigate para navegar a la pantalla por nombre "'ProductsScreen'" esto es igual react router para la navegación programada

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View,Text,Pressable } from 'react-native';
+import { View,Text } from 'react-native';
 import { globalStyles } from '../../styles/globalStyles';
+import { PrimaryButton } from '../../components';
+import { useNavigation } from '@react-navigation/native';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={globalStyles.container}>
         <Text>HomeScreen</Text>
 
-        <Pressable style={globalStyles.primaryButton}>
-            <Text style={globalStyles.buttonText}>Ir a productos</Text>
-        </Pressable>
+        <PrimaryButton label="Productos" onPress={()=> navigation.navigate('ProductsScreen' as never)}/>
     </View>
   );
 };
