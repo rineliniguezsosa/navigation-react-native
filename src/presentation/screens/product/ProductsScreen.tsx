3d2efabf7669a301f760e611e3aa4b97822af7c5
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 14:57:02 -0600

    [FEAT] Multiples cambios en el archivo ProductsScreen.tsx
    - importando el componente FlatList
    - importando los estilos de globalStyles
    - importando el componente PrimaryButton
    - creando el array de objetos Products
    - estableciendo la clase container en el componente View
    - estableciendo la prop style con margen inferior de 10 unidades, tamaño de texto a 30 unidades
    - estableciendo el componente FlatList donde pasamos la data como el array de objetos Products, usamos el renderItem para devolver la lista de los elementos donde desestructuramos el item y devolveremos el componente PrimaryButton con el label igual al valor de la name, establecemos la prop onPress con una función basia por el momento

--- src/presentation/screens/product/ProductsScreen.tsx
+++ src/presentation/screens/product/ProductsScreen.tsx
@@ -1,10 +1,38 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,FlatList } from 'react-native';
+import { globalStyles } from '../../styles/globalStyles';
+import { PrimaryButton } from '../../components';
+
+const Products = [
+  {
+    id:1,name:'Product 1',
+  },
+  {
+    id:2,name:'Product 2',
+  },
+  {
+    id:3,name:'Product 3',
+  },
+  {
+    id:4,name:'Product 4',
+  },
+  {
+    id:5,name:'Product 5',
+  },
+];
 
 export const ProductsScreen = () => {
   return (
-    <View>
-        <Text>ProductsScreen</Text>
+    <View style={globalStyles.container}>
+        <Text style={{marginBottom:10,fontSize:30}}>Products</Text>
+
+        <FlatList
+          data={Products}
+          renderItem={({item})=>(
+            <PrimaryButton label={item.name} onPress={()=> {}} />
+          )}
+        />
     </View>
   );
 };
