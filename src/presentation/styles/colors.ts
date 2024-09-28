5298e6f28ffc0c095110102bc67bd933ce9ec1aa
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 13:50:17 -0600

    [FEAT] Configurando los colores en el objeto globalColors

--- src/presentation/styles/colors.ts
+++ src/presentation/styles/colors.ts
@@ -1,3 +1,9 @@
 export const globalColors = {
-
+    primary:'#7037eb',
+    secondary:'#f72585',
+    tertiary:'#3a0ca3',
+    success:'#4cc9f0',
+    warning:'#fca311',
+    danger:'#e71d36',
+    dark:'#22223B',
 };
