d65ea5a2edfbc600e25cba657c2ee4277aeb1c0c
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 22:23:19 -0600

    [FEAT] Guardando los multiples cambios en el archivo DrawerNavigator
    - deshabilitando exhaustivamente los estilos de linea y el nested component
    - importando DrawerContentComponentProps para el tipado del custom drawer
    - importando DrawerContentScrollView para permitir el scroll dentro del drawer
    - importando DrawerItemList para seguir mostrando el resto de la navegación del drawer
    - importando los colores de globalColors
    - importando el componenet View
    - props en el Drawer.Navigator, drawerContent para devolver la función  CustomDrawer
    - estableciendo la prop screenOptions para customizar la navegación de los componentes StackNavigator y ProfileScreen
    - estableciendo puntos y comas
    - creando la función CustomDrawer donde se considero hacer un avatar

--- src/presentation/routes/DrawerNavigator.tsx
+++ src/presentation/routes/DrawerNavigator.tsx
@@ -1,15 +1,48 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator,DrawerContentComponentProps,DrawerContentScrollView,DrawerItemList } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { ProfileScreen } from '../screens';
+import { globalColors } from '../styles/colors';
+import { View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={(props)=> <CustomDrawer {...props}/>}
+      screenOptions={{
+        headerShown:false,
+        drawerType:'slide',
+        drawerActiveBackgroundColor:globalColors.primary,
+        drawerActiveTintColor:globalColors.primary,
+        drawerItemStyle:{
+            borderRadius:100,
+            paddingHorizontal:20,
+        },
+      }}
+    >
       <Drawer.Screen name="StackNavigator " component={StackNavigator} />
       <Drawer.Screen name="ProfileScreen" component={ProfileScreen} />
     </Drawer.Navigator>
   );
+};
+
+const CustomDrawer = (props:DrawerContentComponentProps) => {
+    return (
+        <DrawerContentScrollView>
+            <View
+                style={{
+                    height:200,
+                    backgroundColor:globalColors.primary,
+                    margin:30,
+                    borderRadius:50,
+                }}
+            />
+
+            <DrawerItemList {...props}/>
+        </DrawerContentScrollView>
+    );
 };
