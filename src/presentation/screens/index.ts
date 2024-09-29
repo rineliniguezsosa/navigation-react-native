a9a0b380cc3b68a0e67d32f897dee7a1eb13627b
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 21:26:50 -0600

    [FEAT] Exportando todo del archivo ProfileScreen en el archivo de barril index.ts

--- src/presentation/screens/index.ts
+++ src/presentation/screens/index.ts
@@ -2,3 +2,4 @@ export * from './home/HomeScreen';
 export * from './product/ProductsScreen';
 export * from './settings/SettingScreen';
 export * from './product/ProductScreen';
+export * from './profile/ProfileScreen';
