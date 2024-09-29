497e691f2fdd9320f64d2d1f952772ffb3cb0d7e
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 21:34:14 -0600

    [FEAT] Cambios en el archivo App.tsx
    - comenatando la importación del StackNavigator
    - importando la navegación de el DrawerNavigator
    - estableciendo en el retorno del componente App el componente DrawerNavigator

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/presentation/routes/StackNavigator';
+// import { StackNavigator } from './src/presentation/routes/StackNavigator'; //if you want to see only the Stacknavigator
+import { DrawerNavigator } from './src/presentation/routes/DrawerNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };
