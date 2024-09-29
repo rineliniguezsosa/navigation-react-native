c1efb7869e490c9961aa2c90099bd89ca0f5a733
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 15:43:09 -0600

    [FEAT] Multiples cambios en el archivo ProfileScreen.tsx
    - comentando exhaustivamente los styles en linea
    - importando el hook  useSafeAreaInsets
    - importando DrawerActions y useNavigation
    - importando el componente  PrimaryButton
    - desestructurando el top de useSafeAreaInsets para que sea dinamico ó seguro el posicionamiento de los margen superior
    - creando la variable navigation donde paasamos por referencia la función useNavigation
    - estableciendo la prop style con los valores flex:1, paddingHorizontal 20 y margintTop: top
    - estableciendo el componente PrimaryButton con el titulo Abrir menú, estableciendo la prop onPress donde establecemos la referencia del la acción para abrir el drawer

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { View,Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { PrimaryButton } from '../../components';
 
 export const ProfileScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
   return (
-    <View>
+    <View style={{flex:1,paddingHorizontal:20,marginTop:top}}>
         <Text>ProfileScreen</Text>
+
+        <PrimaryButton label="Abrir menú" onPress={()=> navigation.dispatch(DrawerActions.toggleDrawer())}/>
     </View>
   );
 };
