7f4509adabb303204d19402578c421f7e4018a53
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 16:16:47 -0600

    [FEAT] Creando las 2 ultimos tabs para el BottonNavihator
    - importando los componentes Tab2Screen,Tab3Screen
    - creando las rutas para mostrar los componentes Tab2Screen,Tab3Screen

--- src/presentation/routes/BottomTabsNavigator.tsx
+++ src/presentation/routes/BottomTabsNavigator.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Tab1Screen } from '../screens';
+import { Tab1Screen,Tab2Screen,Tab3Screen } from '../screens';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,6 +8,8 @@ export const BottomTabsNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
+      <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
+      <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
     </Tab.Navigator>
   );
 };
