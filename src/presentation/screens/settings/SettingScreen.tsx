eedbdedc54466377e0d022e505524ab3a15da944
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 20:50:29 -0600

    [FEAT] Multiples cambios en el archivo SettingScreen
    - importando globalStyles
    - importando useNavigation
    - importando el componente PrimaryButton
    - creando la variable navigation  donde le pasamos la referencia de la función useNavigation
    - estableciendo la clase container al elemento View
    - estableciendo la prop style con el margen inferior a 10 unidades
    - estableciendo el componente PrimaryButton con el lable "Regresar" y la prop onPress con la referencia de la navegación hacia atras

--- src/presentation/screens/settings/SettingScreen.tsx
+++ src/presentation/screens/settings/SettingScreen.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { View,Text } from 'react-native';
+import { globalStyles } from '../../styles/globalStyles';
+import { useNavigation } from '@react-navigation/native';
+import { PrimaryButton } from '../../components';
+
 
 export const SettingScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-        <Text>SettingScreen</Text>
+    <View style={globalStyles.container}>
+        <Text style={{marginBottom:10}}>SettingScreen</Text>
+
+        <PrimaryButton label="Regresar" onPress={()=> navigation.goBack()}/>
     </View>
   );
 };
