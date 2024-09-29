bdded55e0b792798f77329985ced97a802498777
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 17:45:10 -0600

    [FEAT] Estableciendo el componente HamburguesMenu  en el Tab Tab2Screen

--- src/presentation/screens/tabs/Tab2Screen.tsx
+++ src/presentation/screens/tabs/Tab2Screen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View,Text } from 'react-native';
+import { HamburguesMenu } from '../../components';
 
 export const Tab2Screen = () => {
   return (
     <View>
+        <HamburguesMenu/>
         <Text>Tab2Screen</Text>
     </View>
   );
