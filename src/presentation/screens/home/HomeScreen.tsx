c5092690c15a617443e7d60b291410937f08926f
Rinel <rineliniguezsosa@gmail.com>
2024-09-28 21:46:19 -0600

    [FEAT] Cambios en el HomeScrem
    - imoportando el hook useEffect
    - estableciendo la función useEffect donde establecimos un header a la izquierda el cual sera un botón personalizado para abrir el Drawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View,Text } from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { View,Text,Pressable } from 'react-native';
 import { globalStyles } from '../../styles/globalStyles';
 import { PrimaryButton } from '../../components';
-import { useNavigation,NavigationProp } from '@react-navigation/native';
+import { useNavigation,NavigationProp, DrawerActions } from '@react-navigation/native';
 import { RootStackParamList } from '../../../types/interfaces';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerLeft:()=>{
+        <Pressable onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>;
+      },
+    });
+  },[]);
+
   return (
     <View style={globalStyles.container}>
         <Text>HomeScreen</Text>
