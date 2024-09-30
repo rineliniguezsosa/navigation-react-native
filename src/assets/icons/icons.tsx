8f70fece3ac0e9a3e9fd696719b120d498c152d9
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 18:59:30 -0600

    [FEAT] Ceeando el icono por nombre IconAlbumOutline algo que dijiera ó mostrara muchas pantallas para el stack navigator en el bottom tab

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export const IconRocektOutline = () => <Icon name="rocket-outline"/>;
+export const IconAlbumOutline = () => <Icon name="albums-outline"/>;
