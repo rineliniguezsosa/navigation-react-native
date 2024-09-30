2ef7dc5df0c02e97379c2fea6a8bcac7bf6ec275
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 18:56:41 -0600

    [FEAT] Cambios en el archivo Tab1Screen
    - importando el componente  IconRocektOutline
    - estableciendo el componente  IconRocektOutline en el retorno del funcional component Tab1Screen

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View,Text } from 'react-native';
 import { HamburguesMenu } from '../../components';
+import { IconRocektOutline } from '../../../assets';
 
 export const Tab1Screen = () => {
   return (
     <View>
         <HamburguesMenu/>
         <Text>Tab1Screen</Text>
+
+        <IconRocektOutline/>
     </View>
   );
 };
