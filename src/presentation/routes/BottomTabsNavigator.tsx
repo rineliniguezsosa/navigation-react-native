edcfdbb2aabfda73e6be48c49cb9f4fbf3f6a8b6
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 18:24:27 -0600

    [FEAT] Cambios en el archivo BottomTabsNavigator
    - importando TopTabsNavigator
    - estableciendo en el 2da navegación del drawer la navegación superior ó las rutas del TopTabsNavigator

--- src/presentation/routes/BottomTabsNavigator.tsx
+++ src/presentation/routes/BottomTabsNavigator.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react/react-in-jsx-scope */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Tab1Screen,Tab2Screen,Tab3Screen } from '../screens';
+import { Tab1Screen,Tab3Screen } from '../screens';
 import { globalColors } from '../styles/colors';
 import { Text } from 'react-native';
+import { TopTabsNavigator } from './TopTabsNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -28,7 +29,7 @@ export const BottomTabsNavigator = () => {
       }}
     >
       <Tab.Screen name="Tab1Screen" options={{title:'Tab1',tabBarIcon:({color})=> <Text style={{color}}>Tab1</Text>}} component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" options={{title:'Tab2',tabBarIcon:({color})=> <Text style={{color}}>Tab1</Text>}} component={Tab2Screen} />
+      <Tab.Screen name="TopTabsNavigator" options={{title:'Tab2',tabBarIcon:({color})=> <Text style={{color}}>Tab1</Text>}} component={TopTabsNavigator} />
       <Tab.Screen name="Tab3Screen" options={{title:'Tab2',tabBarIcon:({color})=> <Text style={{color}}>Tab1</Text>}} component={Tab3Screen} />
     </Tab.Navigator>
   );
