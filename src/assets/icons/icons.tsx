689b2c5393e887537d3168938da21732d6db5899
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 19:03:19 -0600

    [FEAT] Cambiando el icono IconAlbumOutline por  IconBookOutline
    - cambiando el nombre del icono "albums-outline" por "book-outline"

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export const IconRocektOutline = () => <Icon name="rocket-outline"/>;
-export const IconAlbumOutline = () => <Icon name="albums-outline"/>;
+export const IconBookOutline = () => <Icon name="book-outline"/>;
