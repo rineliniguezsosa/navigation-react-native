a9093dbfe3056e7af503a8beb15289edfc951035
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 18:41:03 -0600

    [FEAT] Cambios en el archivo StackNavigator
    - deshabilitando la exhaustiva dependencia de el hook useEffect
    - importando el useEffect hook
    - creando la variable navigation donde pasamos por referencia la función useNavigation
    - establecemos el hook useEffect donde removimos el header ó Titulo

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,11 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React,{useEffect} from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
 import { HomeScreen,ProductsScreen,SettingScreen,ProductScreen } from '../screens';
 import { RootStackParamList } from '../../types/interfaces';
+import { useNavigation } from '@react-navigation/native';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export const StackNavigator = () => {
+  const navigation = useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShow:false,
+    });
+  },[]);
+
   return (
     <Stack.Navigator>
        <Stack.Screen name="HomeScreen" component={HomeScreen} />
