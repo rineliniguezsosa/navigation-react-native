6c3e68d0fefaa273aad4226039158c80a5fa9275
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 18:29:41 -0600

    [FEAT] Cambios en el TopTabsNavigator
    - importando el componente HamburguesMenu
    - estableciendo un fragment el cual envuelve al componente TopTabsNavigator
    - estableciendo el componente HamburguesMenu en el retorno del componente TopTabsNavigator

--- src/presentation/routes/TopTabsNavigator.tsx
+++ src/presentation/routes/TopTabsNavigator.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { AboutScreen, ProfileScreen } from '../screens';
+import { HamburguesMenu } from '../components';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabsNavigator = () => {
   return (
+    <>
+    <HamburguesMenu/>
     <Tab.Navigator>
       <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
       <Tab.Screen name="AboutScreen" component={AboutScreen} />
     </Tab.Navigator>
+    </>
   );
 };
