1cae89a7606265df82d4e8620b306978fff2594e
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 19:35:13 -0600

    [FEAT] Estableciendo los iconos accessibility-outline y arrow-up-outline

--- src/presentation/routes/BottomTabsNavigator.tsx
+++ src/presentation/routes/BottomTabsNavigator.tsx
@@ -3,7 +3,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens';
 import { globalColors } from '../styles/colors';
-import { Text } from 'react-native';
 import { TopTabsNavigator } from './TopTabsNavigator';
 import { StackNavigator } from './StackNavigator';
 import { IoniIcon } from '../../assets';
@@ -30,8 +29,8 @@ export const BottomTabsNavigator = () => {
         },
       }}
     >
-      <Tab.Screen name="Tab1Screen" options={{title:'Tab1',tabBarIcon:({color})=> <Text style={{color}}>Tab1</Text>}} component={Tab1Screen} />
-      <Tab.Screen name="TopTabsNavigator" options={{title:'Tab2',tabBarIcon:({color})=> <Text style={{color}}>Tab2</Text>}} component={TopTabsNavigator} />
+      <Tab.Screen name="Tab1Screen" options={{title:'Tab1',tabBarIcon:({color})=> <IoniIcon name="accessibility-outline" color={color} size={20} />}} component={Tab1Screen} />
+      <Tab.Screen name="TopTabsNavigator" options={{title:'Tab2',tabBarIcon:({color})=> <IoniIcon name="arrow-up-outline" color={color} size={20} />}} component={TopTabsNavigator} />
       <Tab.Screen name="StackNavigator " options={{title:'Tab3',tabBarIcon:({color})=> <IoniIcon name="book-outline" color={color} size={20} />}} component={StackNavigator} />
     </Tab.Navigator>
   );
