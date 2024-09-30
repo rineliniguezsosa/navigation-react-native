1f2c47ab8563739e26a5fb318eede44e009d17d2
Rinel <rineliniguezsosa@gmail.com>
2024-09-29 19:18:15 -0600

    [FEAT] Corrigiendo la mala definición de la prop coior por color en el fruncional component IoniIcon

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { IoniIconProps } from '../../types/interfaces';
 
-export const IoniIcon = ({name,coior,size}:IoniIconProps) =>{
+export const IoniIcon = ({name,color,size}:IoniIconProps) =>{
     return (
-        <Icon name={name} color={coior} size={size}/>
+        <Icon name={name} color={color} size={size}/>
     );
 };
 
